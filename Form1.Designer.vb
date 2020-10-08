fc4caccf2ec59cc5919d5b5401bb1eaadfb955a7
Auxiliar Desarrollo 03 <AuxDes03@NTRADD3.LOCAL>
2020-10-08 10:48:56 -0500

    funcionalidad basica lee una ruta de una carpeta

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,17 +22,76 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.ListView1 = New System.Windows.Forms.ListView
+        Me.CFDI = New System.Windows.Forms.ColumnHeader
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ProgressBar1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 371)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(909, 53)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "validar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.CFDI})
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(0, 0)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(909, 371)
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'CFDI
+        '
+        Me.CFDI.Text = "Información"
+        Me.CFDI.Width = 907
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(131, 19)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(752, 23)
+        Me.ProgressBar1.TabIndex = 1
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(540, 238)
+        Me.ClientSize = New System.Drawing.Size(909, 424)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Valida Estatus CFDI SAT"
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents CFDI As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,17 +1,32 @@
 ﻿Imports cfdi = estadodecfdi.cfdistatus
+Imports System.Xml
 Public Class Form1
+    Dim Ruta As String = "C:\Users\AuxDes03\Desktop\versionxml\Egresos\"
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Dim estatus = ValidaVigenciaCFDI("GPT181004NV8", "AQC880627A23", "24.97", "3E326422-4AEF-4264-801D-8843AC073015")
-        If estatus = True Then
-            MsgBox("Vigente, Pasa a Flujo")
-        Else
-            MsgBox("cancelado, No pasa a Fujo")
-        End If
+
     End Sub
-
-
+    Private Sub Cargamosxmlsinpasiovo(ByVal Ruta As String)
+        Dim Namefile As String = ""
+        ':::Capturador de errores Try
+        Try
+            ':::Contamos cuanto archivos de texto hay en la carpeta
+            Dim Total = My.Computer.FileSystem.GetFiles(Ruta, FileIO.SearchOption.SearchAllSubDirectories, "*.xml")
+            ':::Realizamos la búsqueda de la ruta de cada archivo de texto y los agregamos al ListBox
+            Dim conter = 0
+            ProgressBar1.Value = conter
+            For Each archivos As String In My.Computer.FileSystem.GetFiles(Ruta, FileIO.SearchOption.SearchAllSubDirectories, "*.xml")
+                Namefile = Replace(archivos, Ruta, "")
+                Call leeXmlSinPasivo(archivos, Namefile)
+                conter = conter + 1
+                ProgressBar1.Value = conter
+            Next
+            ProgressBar1.Value = 100
+        Catch ex As Exception
+            MsgBox("No se realizó la operación por: " & ex.Message)
+        End Try
+    End Sub
     Function ValidaVigenciaCFDI(ByVal RFCEMISOR As String, ByVal RFCRECEPTOR As String, ByVal IMPORTE As String, ByVal UUID As String) As Boolean
-      Try
+        Try
             Dim st As New cfdistatus.ConsultaCFDIServiceClient
             Dim querySend As String
             Dim Acuse
@@ -29,4 +44,163 @@ Public Class Form1
             MsgBox("El Servicio del SAT No esta Activo")
         End Try
     End Function
+
+#Region "EXTRAEMOS LOS DATOS DE CADA XML PARA REVISAR SI YA SE ENCUANTRA UN PASIVO PARA LOS PENDIENTES DE CAPTURA"
+    Private Sub leeXmlSinPasivo(ByVal ruta As String, ByVal FileName As String)
+        Try
+            Dim Version_xml As String = ""
+            Dim implocaltraladados As Double = 0
+            Dim implocalretenidos As Double = 0
+            Dim retenidos As Double = 0
+            Dim trasladados As Double = 0
+            Dim Subtotal As Double = 0
+            Dim valor_noIdentificacion As String = ""
+            Dim preciounitarioxml As Double = 0
+            Dim importeconcepto As Double = 0
+            Dim cantidadvendida As Double = 0
+            Dim subtotalclave As Decimal = 0
+            Dim descripcionxml As String = ""
+            Dim Emisor_Nombre As String = ""
+            Dim Emisor_Rfc As String = ""
+            Dim Receptor_UsoCFDI As String = ""
+            Dim total As Double = 0
+            Dim Serie As String = ""
+            Dim Folio As String = ""
+            Dim SerieFolio As String = ""
+            Dim UUID As String = ""
+            Dim MetodoPago As String = ""
+            Dim TipoDeComprobante As String = ""
+
+            Dim Emisor_RegimenFiscal As String = ""
+            Dim Fecha As String = ""
+            Dim Sello As String = ""
+            Dim NoCertificado As String = ""
+            Dim Certificado As String = ""
+            Dim Moneda As String = ""
+            Dim LugarExpedicion As String = ""
+            Dim Receptor_Rfc As String = ""
+            Dim Receptor_Nombre As String = ""
+            Dim FechaTimbrado As String = ""
+            Dim RfcProvCertif As String = ""
+            Dim SelloCFD As String = ""
+            Dim NoCertificadoSAT As String = ""
+            Dim SelloSAT As String = ""
+            Dim CtaBeneficiario As String = ""
+            Dim CtaOrdenante As String = ""
+            Dim FechaPago As String = ""
+            Dim FormaDePagoP As String = ""
+            Dim MonedaP As String = ""
+            Dim Monto As String = 0
+            Dim RfcEmisorCtaBen As String = ""
+            Dim RfcEmisorCtaOrd As String = ""
+            Dim IdDocumento As String = ""
+            Dim Version As String = ""
+            Dim FormaPago As String = ""
+
+
+            Dim documentoxml As XmlDocument
+            documentoxml = New XmlDocument
+            Dim VarManager As XmlNamespaceManager = New XmlNamespaceManager(documentoxml.NameTable)
+            documentoxml.Load(Trim(ruta))
+
+            Dim root As XmlElement = documentoxml.DocumentElement
+            Dim nodeToFind As XmlNode
+
+            VarManager.AddNamespace("cfdi", "http://www.sat.gob.mx/cfd/3")
+            VarManager.AddNamespace("tfd", "http://www.sat.gob.mx/TimbreFiscalDigital")
+            VarManager.AddNamespace("implocal", "http://www.sat.gob.mx/implocal")
+            VarManager.AddNamespace("pago10", "http://www.sat.gob.mx/Pagos")
+
+            Dim archivo As String = FileName
+            Version_xml = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Version", VarManager).InnerText
+            TipoDeComprobante = documentoxml.SelectSingleNode("/cfdi:Comprobante/@TipoDeComprobante", VarManager).InnerText
+            'DATOS GENERALES COMPROBANTE
+            nodeToFind = root.SelectSingleNode("/cfdi:Comprobante/@Serie", VarManager)
+            If nodeToFind Is Nothing Then
+                Serie = ""
+            Else
+                Serie = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Serie", VarManager).InnerText
+            End If
+
+            Dim nodeToFolio As XmlNode = root.SelectSingleNode("/cfdi:Comprobante/@Folio", VarManager)
+            If nodeToFolio Is Nothing Then
+                Folio = ""
+            Else
+                Folio = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Folio", VarManager).InnerText
+            End If
+
+            Dim nodeToMetodoPago As XmlNode = root.SelectSingleNode("/cfdi:Comprobante/@MetodoPago", VarManager)
+            If nodeToMetodoPago Is Nothing Then
+                MetodoPago = ""
+            Else
+                MetodoPago = documentoxml.SelectSingleNode("/cfdi:Comprobante/@MetodoPago", VarManager).InnerText
+            End If
+
+            Dim nodeToFormaPago As XmlNode = root.SelectSingleNode("/cfdi:Comprobante/@FormaPago", VarManager)
+            If nodeToFormaPago Is Nothing Then
+                FormaPago = ""
+            Else
+                FormaPago = documentoxml.SelectSingleNode("/cfdi:Comprobante/@FormaPago", VarManager).InnerText
+            End If
+
+            SerieFolio = Trim(Serie & Folio)
+            Fecha = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Fecha", VarManager).InnerText
+            Sello = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Sello", VarManager).InnerText
+            NoCertificado = documentoxml.SelectSingleNode("/cfdi:Comprobante/@NoCertificado", VarManager).InnerText
+            Certificado = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Certificado", VarManager).InnerText
+
+            Subtotal = documentoxml.SelectSingleNode("/cfdi:Comprobante/@SubTotal", VarManager).InnerText
+            Moneda = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Moneda", VarManager).InnerText
+            Certificado = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Certificado", VarManager).InnerText
+            total = documentoxml.SelectSingleNode("/cfdi:Comprobante/@Total", VarManager).InnerText
+            LugarExpedicion = documentoxml.SelectSingleNode("/cfdi:Comprobante/@LugarExpedicion", VarManager).InnerText
+
+            'DATOS GENERALES EMISOR
+            Emisor_RegimenFiscal = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Emisor/@RegimenFiscal", VarManager).InnerText
+            Emisor_Rfc = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Emisor/@Rfc", VarManager).InnerText
+            Emisor_Nombre = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Emisor/@Nombre", VarManager).InnerText
+
+            'DATOS GENERALES RECEPTOR
+            Receptor_Rfc = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Receptor/@Rfc", VarManager).InnerText
+            Receptor_UsoCFDI = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Receptor/@UsoCFDI", VarManager).InnerText
+
+            Dim nodeToNombre As XmlNode = root.SelectSingleNode("/cfdi:Comprobante/cfdi:Receptor/@Nombre", VarManager)
+
+            If nodeToNombre Is Nothing Then
+                Receptor_Nombre = ""
+            Else
+                Receptor_Nombre = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Receptor/@Nombre", VarManager).InnerText
+            End If
+
+            'DATOS GENERALES COMPLEMENTO
+            UUID = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Complemento/tfd:TimbreFiscalDigital/@UUID", VarManager).InnerText
+            FechaTimbrado = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Complemento/tfd:TimbreFiscalDigital/@FechaTimbrado", VarManager).InnerText
+            RfcProvCertif = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Complemento/tfd:TimbreFiscalDigital/@RfcProvCertif", VarManager).InnerText
+            SelloCFD = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Complemento/tfd:TimbreFiscalDigital/@SelloCFD", VarManager).InnerText
+            NoCertificadoSAT = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Complemento/tfd:TimbreFiscalDigital/@NoCertificadoSAT", VarManager).InnerText
+            SelloSAT = documentoxml.SelectSingleNode("/cfdi:Comprobante/cfdi:Complemento/tfd:TimbreFiscalDigital/@SelloSAT", VarManager).InnerText
+
+            '----------------------------------------------------------------------------------------------------'
+            Dim item As New ListViewItem()
+
+            Dim estatus = ValidaVigenciaCFDI(Emisor_Rfc, Receptor_Rfc, total, UUID)
+            If estatus = True Then
+                item.Text = "CFDI ( VIGENTE ) Emisor: (" & Emisor_Rfc & ") Receptor: (" & Receptor_Rfc & ") Total: (" & total & ") UUID: " & UUID
+                item.ForeColor = Color.Blue
+                ListView1.Items.Add(item)
+            Else
+               item.Text = "CFDI (CANCELADO) Emisor: (" & Emisor_Rfc & ") Receptor: (" & Receptor_Rfc & ") Total: (" & total & ") UUID: " & UUID
+                item.ForeColor = Color.Red
+                ListView1.Items.Add(item)
+            End If
+            '----------------------------------------------------------------------------------------------------'
+
+        Catch ex As Exception
+            MessageBox.Show("error", ex.Message)
+        End Try
+    End Sub
+#End Region
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Cargamosxmlsinpasiovo(Ruta)
+    End Sub
 End Class
